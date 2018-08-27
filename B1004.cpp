4329d5069c6f63b62b5ab6794ed808863871ad75
FrankinZhang <ztc19980720@gmai.com>
2018-08-27 12:02:14 +0800

    add B1004.cpp

---
+++ B1004.cpp
@@ -0,0 +1,23 @@
+#include<cstdio>
+struct Student{
+    char name[15];
+    char id[15];
+    int score;
+}temp, ans_max,ans_min;
+int main()
+{
+    int m;
+    scanf("%d",&m);
+    ans_max.score = -1;
+    ans_min.score = 101;
+    for(int i=0;i<m;i++){
+        scanf("%s%s%d",&temp.name,&temp.id,&temp.score);
+        if(temp.score>ans_max.score) ans_max = temp;
+        if(temp.score<ans_min.score) ans_min = temp;
+    
+    }
+    printf("%s %s\n",ans_max.name,ans_max.id);
+    printf("%s %s\n",ans_min.name,ans_min.id);
+
+    return 0;
+}
